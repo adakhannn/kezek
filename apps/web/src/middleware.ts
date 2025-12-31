0fe8c3be06c777827d68c15be7d9bb3157659a86
adakhannn <osorovadahan04@gmail.com>
2026-01-01 01:04:05 +0600

    Обновлен matcher в middleware для работы на всех страницах

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -106,5 +106,14 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/',], // можно добавить: '/auth/sign-in', '/auth', ...
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - public folder
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
 };
